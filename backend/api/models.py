03aa840199c2e31b33b24ef725852996de193803
hadisiswanto62 <hadi0006@e.ntu.edu.sg>
2021-08-07 16:35:56 +0800

    added model for tx and txgroup

--- backend/api/models.py
+++ backend/api/models.py
@@ -4,3 +4,18 @@ from django.contrib.auth.models import User
 # Create your models here.
 class UserModel(User):
     discord_id = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+class TransactionGroup(models.Model):
+    desc = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+class Transaction(models.Model):
+    sender = models.ForeignKey(UserModel, on_delete=models.CASCADE)
+    receiver = models.ForeignKey(UserModel, on_delete=models.CASCADE)
+    amount = models.FloatField()
+    is_paid = models.BooleanField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
